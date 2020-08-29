c4cc11612cc124ea399c7ce62c9f43b67c544ec4
davawen <56738724+davawen@users.noreply.github.com>
2020-08-29 03:27:17 +0200

    ressayer embed, ne marche pas

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -10,13 +10,25 @@ module.exports = {
 		
 		const members = role_mention.members.map(m => m.user);
 			
+		/*let embed = new MessageEmbed()
+			.setColor("#d54e12")
+			.setTitle("Role")*/
+		
 		members.forEach(
 			m =>
 			{
+				/*embed.addField(
+					{
+						name: m.username,
+						value: m.avatarURL(),
+						inline: false
+					}
+				)*/
+					
 				message.channel.send(m.username, {files: [m.avatarURL()]});
 			}
 		);
 		
-		
+		//message.channel.send(embed);
 	}
 }
