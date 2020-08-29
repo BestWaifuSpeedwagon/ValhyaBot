528e9049f52d3354ed6ef909286a26f4db378372
davawen <56738724+davawen@users.noreply.github.com>
2020-08-29 18:29:41 +0200

    changer à administrateur

--- commands/roles/add.js
+++ commands/roles/add.js
@@ -1,18 +1,24 @@
-module.exports.run = (client, message, args) => {
+module.exports.run = (client, message, args) =>
+{
     const role = message.guild.roles.cache.find(role => role.name === args.toString());
-    if (role) {
+    
+    if (role)
+    {
         if (message.member.roles.cache.has(role.id)) return message.channel.send("Vous avez déjà ce rôle !");
-        if (role.permissions.has('KICK_MEMBERS')) return message.channel.send("Vous ne pouvez pas avoir ce rôle !");
+        if (role.permissions.has('ADMINISTRATOR')) return message.channel.send("Vous n'êtes pas administrateur!");
 
         message.member.roles.add(role)
             .then(m => message.channel.send(`Vous possédez maintenant le role ${role}.`))
             .catch(e => console.log(e));
-    } else {
+    }
+    else
+    {
         message.channel.send("Le rôle n'existe pas !");
     }
 }
 
-module.exports.help = {
+module.exports.help =
+{
     name: "add",
     description: "Ajouter un role",
     usage: "<role à ajouter>",
