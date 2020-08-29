1cdbe3b5b6fd1018b767d26cd8122ca30184572a
Kamagyu <nathan.damien1972@gmail.com>
2020-08-29 12:01:51 +0200

    Added commands :
    add.js to give yourself several roles
    remove.js to remove a role.
    removes.js to remove multiple roles.

---
+++ commands/remove.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'remove',
+    description: 'Supprimer un role',
+    execute(client, message, args) {
+        const role = message.guild.roles.cache.find(role => role.name === args.toString());
+        if (role) {
+            if (!message.member.roles.cache.has(role.id)) return message.channel.send("Vous n'avez pas ce role !");
+
+            message.member.roles.remove(role)
+                .then(m => message.channel.send(`Vous ne possédez plus le role ${role}.`))
+                .catch(e => console.log(e));
+        } else {
+            message.channel.send("Le rôle n'existe pas !");
+        }
+    }
+}
