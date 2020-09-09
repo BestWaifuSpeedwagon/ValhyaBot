d04ead76cbe26ca3cb38f587d8dc963f1d6bff17
Kamagyu <nathan.damien1972@gmail.com>
2020-09-09 16:33:10 +0200

    Added ayaya command :kappa:

---
+++ commands/misc/ayaya.js
@@ -0,0 +1,11 @@
+module.exports.run = (client, message, args) =>
+{
+    message.channel.send("https://www.youtube.com/watch?v=9wnNW4HyDtg");
+}
+
+module.exports.help =
+{
+    name: "ayaya",
+    description: "Renvoie ayaya !",
+    args: false
+};
