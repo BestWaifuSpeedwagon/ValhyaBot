51ebe4dedac16fbbf3ed8d5221d6bd36d5c8471e
davawen <56738724+davawen@users.noreply.github.com>
2020-09-09 22:53:48 +0200

    Rechangé .gitignore

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 //Charge le .env dans process.env
+
 const config = 
 {
     TOKEN: process.env.TOKEN,
     PREFIX: process.env.PREFIX,
     twitchID: process.env.twitchID
 }
-
 
 const { Client, Collection, ClientApplication } = require('discord.js');
 const fs = require('fs');
