c13895ad217eeca929821cb51c8e14415fce837f
davawen <56738724+davawen@users.noreply.github.com>
2020-09-12 13:46:53 +0200

    Mis database dans le dossier data et changé userinfo pour être compatible avec des array

--- commands/misc/userinfo.js
+++ commands/misc/userinfo.js
@@ -1,20 +1,43 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, Client, Message } = require("discord.js");
+const database = require('../../data/database.json');
+
+/**
+ * 
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {string[]} args 
+ */
 
 exports.run = (client, message, args) =>
 {
-    const user_mention = message.mentions.users.first();
-    const str = user_mention === undefined ? "n'existe pas." : user_mention.username;
-    let embed = new MessageEmbed()
-        .setColor('#008000')
-        .setTitle(`Les informations de ${str} !`)
-        .setThumbnail('https://cdn.discordapp.com/attachments/744622330117881926/754289447352139806/FO2p50F7Nf4AAAAASUVORK5CYII.png')
-        .setURL('https://github.com/BestWaifuSpeedwagon/ValhyaBot')
-        .addFields(
-            {
-                name: 'Niveau :',
-                value: `[niveau]`
-            }
-        )
+    //Transforme tout les argument en minuscules
+    args.forEach(a => a = a.toLowerCase());
+    
+    //Obtiens tous les membre dans les arguments
+    const user_mention = message.guild.members.cache.filter(m => args.includes(m.nickname.toLowerCase()));
+    
+
+    user_mention.forEach(
+        user =>
+        {
+            let embed = new MessageEmbed()
+                .setColor('#008000')
+                .setTitle(`Informations sur ${user.nickname} !`)
+                .setThumbnail('https://cdn.discordapp.com/attachments/744622330117881926/754289447352139806/FO2p50F7Nf4AAAAASUVORK5CYII.png')
+                .setURL('https://github.com/BestWaifuSpeedwagon/ValhyaBot')
+            
+            embed.addField('Niveau: ', `${user.user.username}`)
+        }
+        
+    );
+    
+    
+        // .addFields(
+        //     {
+        //         name: 'Niveau :',
+        //         value: `[niveau]`
+        //     }
+        // )
 
     message.channel.send(embed);
     
